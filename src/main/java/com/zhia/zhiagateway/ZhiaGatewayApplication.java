843d4086d7017081d29eabe74728882cb3a23d5d
lizhi <18345339912@163.com>
2023-05-19 16:54:18 +0800

    Initial commit

---
+++ src/main/java/com/zhia/zhiagateway/ZhiaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.zhia.zhiagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhiaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhiaGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhia/zhiagateway/ZhiaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhia.zhiagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhiaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
